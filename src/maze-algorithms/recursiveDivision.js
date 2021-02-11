5a37d64c7d7c2985c892b48c583f2437a910a4aa
Sethuram.S.V <sethuram52001@gmail.com>
2021-02-11 22:39:21 +0530

    refactored a little bit, still recursiveDivision needs refactoring

--- src/maze-algorithms/recursiveDivision.js
+++ src/maze-algorithms/recursiveDivision.js
@@ -1,30 +1,22 @@
 let walls;
+
 export function recursiveDivisionMaze(grid, startNode, finishNode) {
-  if (!startNode || !finishNode || startNode === finishNode) {
-    return false;
-  }
-  let vertical = range(grid[0].length);
-  let horizontal = range(grid.length);
+  // if start or finish node skip it
+  if (!startNode || !finishNode)
+    return;
+  // arrays with values of grid dimensions
+  let vertical = Array(grid[0].length).fill().map((_, i) => i);
+  let horizontal = Array(grid.length).fill().map((_, i) => i);
+  // initializing the walls global variable to an empty array
   walls = [];
+  // main func
   getRecursiveWalls(vertical, horizontal, grid, startNode, finishNode);
   return walls;
 }
 
-function range(len) {
-  let result = [];
-  for (let i = 0; i < len; i++) {
-    result.push(i);
-  }
-  return result;
-}
-
-//dir === 0 => Horizontal
-//dir === 1 => Vertical
-
 function getRecursiveWalls(vertical, horizontal, grid, startNode, finishNode) {
-  if (vertical.length < 2 || horizontal.length < 2) {
+  if (vertical.length < 2 || horizontal.length < 2)
     return;
-  }
   let dir;
   let num;
   if (vertical.length > horizontal.length) {
@@ -36,81 +28,53 @@ function getRecursiveWalls(vertical, horizontal, grid, startNode, finishNode) {
     num = generateOddRandomNumber(horizontal);
   }
 
+  // recursive part where the approach to 
+  // start horizontal or vertical is dependent on dir variable
   if (dir === 0) {
     addWall(dir, num, vertical, horizontal, startNode, finishNode);
-    getRecursiveWalls(
-      vertical.slice(0, vertical.indexOf(num)),
-      horizontal,
-      grid,
-      startNode,
-      finishNode
-    );
-    getRecursiveWalls(
-      vertical.slice(vertical.indexOf(num) + 1),
-      horizontal,
-      grid,
-      startNode,
-      finishNode
-    );
-  } else {
+    getRecursiveWalls(vertical.slice(0, vertical.indexOf(num)), horizontal, grid, startNode, finishNode);
+    getRecursiveWalls(vertical.slice(vertical.indexOf(num) + 1), horizontal, grid, startNode, finishNode);
+  }
+  else {
     addWall(dir, num, vertical, horizontal, startNode, finishNode);
-    getRecursiveWalls(
-      vertical,
-      horizontal.slice(0, horizontal.indexOf(num)),
-      grid,
-      startNode,
-      finishNode
-    );
-    getRecursiveWalls(
-      vertical,
-      horizontal.slice(horizontal.indexOf(num) + 1),
-      grid,
-      startNode,
-      finishNode
-    );
+    getRecursiveWalls(vertical, horizontal.slice(0, horizontal.indexOf(num)), grid, startNode, finishNode);
+    getRecursiveWalls(vertical, horizontal.slice(horizontal.indexOf(num) + 1), grid, startNode, finishNode);
   }
 }
 
-function generateOddRandomNumber(array) {
-  let max = array.length - 1;
-  let randomNum =
-    Math.floor(Math.random() * (max / 2)) +
-    Math.floor(Math.random() * (max / 2));
+// generates a random number which is odd
+function generateOddRandomNumber(arr) {
+  let max = arr.length - 1;
+  let randomNum = Math.floor(Math.random() * (max / 2)); 
   if (randomNum % 2 === 0) {
-    if (randomNum === max) {
+    if (randomNum === max)
       randomNum -= 1;
-    } else {
+    else
       randomNum += 1;
-    }
   }
-  return array[randomNum];
-}
+  return arr[randomNum];
+} 
 
-//dir === 0 => Horizontal
-//dir === 1 => Vertical
-
+// func to push the coordinates of nodes into wall array
 function addWall(dir, num, vertical, horizontal, startNode, finishNode) {
   let isStartFinish = false;
   let tempWalls = [];
   if (dir === 0) {
-    if (horizontal.length === 2) return;
+    if (horizontal.length === 2)
+      return;
     for (let temp of horizontal) {
-      if (
-        (temp === startNode.row && num === startNode.col) ||
-        (temp === finishNode.row && num === finishNode.col)
-      ) {
+      if ((temp === startNode.row && num === startNode.col) ||(temp === finishNode.row && num === finishNode.col)) {
         isStartFinish = true;
         continue;
       }
       tempWalls.push([temp, num]);
     }
-  } else {
-    if (vertical.length === 2) return;
+  }
+  else {
+    if (vertical.length === 2)
+      return;
     for (let temp of vertical) {
-      if (
-        (num === startNode.row && temp === startNode.col) ||
-        (num === finishNode.row && temp === finishNode.col)
-      ) {
+      if ((num === startNode.row && temp === startNode.col) ||(num === finishNode.row && temp === finishNode.col)) {
         isStartFinish = true;
         continue;
       }
@@ -125,14 +89,14 @@ function addWall(dir, num, vertical, horizontal, startNode, finishNode) {
   }
 }
 
+// func to generate a random number
 function generateRandomNumber(max) {
-  let randomNum =
-    Math.floor(Math.random() * (max / 2)) +
-    Math.floor(Math.random() * (max / 2));
+  let randomNum = Math.floor(Math.random() * (max / 2));
   if (randomNum % 2 !== 0) {
     if (randomNum === max) {
       randomNum -= 1;
-    } else {
+    }
+    else {
       randomNum += 1;
     }
   }
